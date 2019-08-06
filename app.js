f9634da774ce263dd2f0640a74f30b762c2fdd97
ponpon123987 <52985943+ponpon123987@users.noreply.github.com>
2019-08-06 15:23:18 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -3,26 +3,18 @@ const express = require('express')
 const mysql = require('mysql');
 const fs = require('fs');
 
-const dbName = 'mongodb://b5e5398f-f1cb-4c6d-9ec2-23407a72647e:9Zz3AZDvpzkc3C7iev0WliKAT@40.83.78.152:27017/401612f6-2c0f-4596-b446-a242dda9e16c'
-mongoose.connect(dbName, { useNewUrlParser: true })
-   .then(() => console.log('Connected to the MongoDB...'))
-   .catch(err => console.log('Could not connect to MongoDB...', err));
-
 const db = mongoose.connection;
-// mongoose.connect("mongodb://localhost:27017/world", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/ward", { useNewUrlParser: true });
 
+//check db connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+})
     
-// //check db connection
-// db.once('open', function(){
-//     console.log('Connected to MongoDB');
-// })
-    
-// //check for db errors
-// db.on('error', function(err){
-//     console.log(err);
-// })
-
-const collectionName = db.collection('hbt');
+//check for db errors
+db.on('error', function(err){
+    console.log(err);
+})
 
 //create connection to local MySQL DB
 const connection = mysql.createConnection({
@@ -46,16 +38,6 @@ app.get('/', function(req,res){
     res.sendStatus(200);
 })
 
-////If you wish to reconstruct your schema you may set up your own schema
-// const hbtSchema = new mongoose.Schema({
-//     patient: String,
-//     heartbeat: Number,
-//     ts: Date,
-// });
-
-////Remember to use the schema you set if you had set one
-//const hbtData = mongoose.model('hbt', hbtSchema);
-
 //set an api route to check if DB has data
 app.get('/hbt', function(req,res){
     hbtData.find({}, function(err,hbts){
@@ -69,7 +51,9 @@ app.get('/hbt', function(req,res){
     })
 })
 
+const collectionName = db.collection('hbt');
 const fileName = 'test.json';
+
 //Query MySQL DB
 connection.query('select * from `heartbeat`', function(err,results,fields){
     if(err){throw err;}
@@ -79,27 +63,9 @@ connection.query('select * from `heartbeat`', function(err,results,fields){
         console.log('Saved!');
     });
     ////////////////////////////////////////////////////////////////
-    console.log(results);
     const parsedData = JSON.parse(JSON.stringify(results));
     const propCount = Object.keys(results).length;
 
-    // //get the entries with a for loop and save
-    // for(var i=0;i<propCount;i++){
-    //     ////If you use your own schema, remember to parse your data into your schema then save
-    //     // var newHbt = new hbtData({
-    //     //     patient: parsedData[i].patient,
-    //     //     heartbeat: parsedData[i].heartbeat,
-    //     //     ts: parsedData[i].ts
-    //     // });
-    //     // newHbt.save(function(err){
-    //     //     if(err){console.log(err);}
-    //     // }); 
-        
-    //     parsedData[i].ts = new Date(parsedData[i].ts);
-    //     collectionName.insertOne(parsedData[i],function(err){
-    //         if(err){console.log(err);}
-    //     });
-    // }
     for(var i=0;i<propCount;i++){
         parsedData[i].ts = new Date(parsedData[i].ts);
     };
